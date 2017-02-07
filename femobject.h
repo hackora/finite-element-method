722aa177ef9d99a7000019adc1e76910f777fc58
ghada <gbo013@post.uit.no>
2017-02-07 12:43:42 +0100

    FEMObject class  members added

--- femobject.h
+++ femobject.h
@@ -2,11 +2,21 @@
 #define FEMOBJECT_H
 
 #include <gmTrianglesystemModule>
+#include "node.h"
 
 class FEMObject : public GMlib::TriangleFacets<float>
 {
 public:
     FEMObject();
+
+    ArrayLX<Node>           _nodes;
+    GMlib::DMatrix<float>   _A;
+    GMlib::DVector<float>   _b;
+
+    void regularTriangulation(int,int,float);
+    void randomTriangulation(int,float);
+    void updateHeight(float);
+    //combination method I'm not sure about the signature yet
 };
 
 #endif // FEMOBJECT_H
