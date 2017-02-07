ef9c1f7048c657f6ea35633996e203c2ded98a61
ghada <gbo013@post.uit.no>
2017-02-07 13:04:25 +0100

    implemented the regular triangulation method

--- femobject.cpp
+++ femobject.cpp
@@ -4,3 +4,16 @@ FEMObject::FEMObject()
 {
 
 }
+
+void FEMObject::regularTriangulation(int n, int m, float r){
+    auto centerPoint = GMlib::Point<float,2>(0,0);
+    this->insertAlways(centerPoint);
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n*(i+1);j++){
+            GMlib::Angle a = (j * M_2PI)/(n*(i+1));
+            GMlib::SqMatrix<float,2> mt(a,GMlib::Vector<float,2>(1,0),GMlib::Vector<float,2>(0,1));
+            GMlib::Point<float,2> pt = mt * GMlib::Point<float,2>((i+1)/m*r/(j+1),0);
+            this->insertAlways(pt);
+        }
+    }
+}
